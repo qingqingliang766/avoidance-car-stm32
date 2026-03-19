90cbfff427d24687b022dd56bd2299ee415b44f4
qingqingliang766 <2401752003@qq.com>
2026-03-19 20:21:37 +0800

    写个个倒车前进的test，成功运行，电源接口不太稳定

--- test/pwm_test.c
+++ test/pwm_test.c
@@ -1,6 +1,6 @@
 #include "pwm_test.h"
 #include "app_pwm.h"
-#include "delay.h" // 引入山羊大佬的库
+#include "delay.h"
 
 // @简介：对PWM模块进行测试
 void PWM_Test(void) 
@@ -9,19 +9,31 @@ void PWM_Test(void)
     
     App_PWM_Cmd(1); // 唤醒电机
 
-    App_PWM_Set_L(30); 
-    App_PWM_Set_R(30); 
-    Delay(5000); // 完美调用大佬的延时！
+    App_PWM_Set_L(50); 
+    App_PWM_Set_R(50); 
+    Delay(5000);
     
-    App_PWM_Set_L(40); 
-    App_PWM_Set_R(40); 
+    App_PWM_Set_L(50); 
+    App_PWM_Set_R(-50); 
     Delay(2000); 
     
-    App_PWM_Set_L(90); 
-    App_PWM_Set_R(90); 
+    App_PWM_Set_L(70); 
+    App_PWM_Set_R(70); 
+    Delay(2000);
+
+		App_PWM_Set_L(50); 
+    App_PWM_Set_R(-50); 
+    Delay(2000); 
+    
+    App_PWM_Set_L(70); 
+    App_PWM_Set_R(70); 
+    Delay(2000); 
+		
+		App_PWM_Set_L(100); 
+    App_PWM_Set_R(100); 
     Delay(2000); 
     
     App_PWM_Cmd(0); // 休眠电机
-    // 注意：把 while(1){} 删掉了！测完就溜，把控制权还给主函数！
+    // 注意：把 while(1){} 
 }
 
